71c3d27b457a73151ac85911a2633c7b7160aa80
haijing-zhang <56798220+haijing-zhang@users.noreply.github.com>
2023-01-25 08:21:55 -0800

    Create FavoriteRequestBody.java

---
+++ twitchbe/java/com/haijing/jupiter/Entity/request/FavoriteRequestBody.java
@@ -0,0 +1,15 @@
+package com.haijing.jupiter.entity.request;
+
+import com.haijing.jupiter.entity.db.Item;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FavoriteRequestBody {
+
+    @JsonProperty("favorite")
+    private Item favoriteItem;
+
+    public Item getFavoriteItem() {
+        return favoriteItem;
+    }
+}
+
